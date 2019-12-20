2b5c3b23b4c1f3564f5ba87780f30088636f845d
Linus Falk <linfa084@student.liu.se>
2019-12-20 03:04:10 +0100

    Like and Unlike Scream

---
+++ socialapp-client/src/redux/reducers/dataReducer.js
@@ -0,0 +1,54 @@
+import {
+  SET_SCREAMS,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM,
+  LOADING_DATA
+} from "../types";
+
+const initialState = {
+  screams: [],
+  scream: {},
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case LOADING_DATA:
+      return {
+        ...state,
+        loading: true
+      };
+    case SET_SCREAMS:
+      return {
+        ...state,
+        screams: action.payload,
+        loading: false
+      };
+    case LIKE_SCREAM:
+      console.log(state);
+      let index = state.screams.findIndex(
+        scream => scream.screamId === action.payload.screamId
+      );
+      state.screams[index] = action.payload;
+      if (state.scream.screamId === action.payload.screamId) {
+        state.scream = action.payload;
+      }
+      return {
+        ...state
+      };
+    case UNLIKE_SCREAM:
+      console.log(state);
+      let idx = state.screams.findIndex(
+        scream => scream.screamId === action.payload.screamId
+      );
+      state.screams[idx] = action.payload;
+      if (state.scream.screamId === action.payload.screamId) {
+        state.scream = action.payload;
+      }
+      return {
+        ...state
+      };
+    default:
+      return state;
+  }
+}
